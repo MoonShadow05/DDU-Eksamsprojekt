c511ccca14ff5af3842e4faea982bce4f1c0101c
MoonShadow <kean05olsen@gmail.com>
2025-04-28 10:51:58 +0200

    Movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.InputSystem; // Required for the new Input System
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5f; // Movement speed
+    
+
+    void Update()
+    {
+        
+    }
+
+    // This method is called by the Input System when movement input is detected
+    
+}
