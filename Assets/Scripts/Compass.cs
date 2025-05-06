2226d965b9d1dca591578762dfb7868abd865d89
DoktorBamse <doktorbamse2620@gmail.com>
2025-05-06 13:28:45 +0200

    Compass part 1

---
+++ Assets/Scripts/Compass.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Compass : MonoBehaviour
+{
+    public RawImage compassImage;
+    public Transform player;
+
+    private void Update()
+    {
+        compassImage.uvRect = new Rect(player.localEulerAngles.y / 360f, 0f, 1f, 1f);
+    }
+}
